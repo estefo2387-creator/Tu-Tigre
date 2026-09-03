42602611ca4e1463d1f7e12c98593c2a5fe0798e
Estefo <celestepadillpardo16.com>
2026-09-03 16:45:49 -0400

    yuyuyuy

---
+++ 8.Clase_03_09_26/js/ejemplo2.js
@@ -0,0 +1,29 @@
+let a=5;
+a+=a;
+console.log(a);
+let x=3,y=4
+console.log("x=",x,"y=",y);
+console.log("x+y=",x+y);
+// x/y es una divicion
+console.log("x/y=",x/y);
+//x**y es una potencia
+console.log("x**y=",x**y);
+//x%y es el residuo de la divicion
+console.log("x%y=",x%y);
+let anno=prompt("idependencia de sannta cruz")
+if(anno==1810)
+{console.log("correcto")}
+else
+{console.log("incorrecto")}
+// escribir un programa que pida al usuario ingresar un numero y determinar si ese numero es par o impar
+function ParImpar(n) {
+if(n % 2==0)
+{console.log("es par")}
+else
+{console.log("es impar")}
+}
+let n=Number(prompt("introduzca un numero"))
+ParImpar(n)
+
+
+
---
+++ 8.Clase_03_09_26/js/miScript.js
@@ -0,0 +1,19 @@
+let age=prompt("Ingrese su edad");
+alert("you are" + age + " years old");
+let res=confirm("eres mayor de edad??????");
+if (res) {
+    alert("Eres mayor de edad");
+} 
+else {
+    alert("No eres mayor de edad");
+}
+alert(Number("12345"));
+alert(Number("12345gt"));
+alert(Number("true"));
+alert(Number("false"));
+
+alert(Boolean("1"));
+alert(Boolean("0"));
+
+alert(Boolean("holo"));
+alert(Boolean(""));
