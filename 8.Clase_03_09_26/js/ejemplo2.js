69a3056a6f303abc161f998431481f463daa3fd4
Estefo <celestepadillpardo16.com>
2026-09-03 18:01:32 -0400

    2344234

--- 8.Clase_03_09_26/js/ejemplo2.js
+++ 8.Clase_03_09_26/js/ejemplo2.js
@@ -24,6 +24,26 @@ else
 }
 let n=Number(prompt("introduzca un numero"))
 ParImpar(n)
+function comprar(producto, cantidad) {
+    let precios ={
+        teclados: 100,
+        mouse: 50,
+        monitor: 300,
+        audifonos: 150,
+        laptop: 500,
+    }
+    if (precios[producto] !== undefined) {
+        let total = precios[producto] * cantidad;
+       console.log("producto: " + producto);
+       console.log("cantidad: " + cantidad);
+       console.log("total: " + total);
+    } else {
+        console.log("Producto no disponible");
+    }
+}
 
+let producto = prompt("Ingrese el producto que desea comprar (teclados, mouse, monitor, audifonos, laptop):");
+let cantidad = parseInt(prompt("Ingrese la cantidad que desea comprar:"));
+comprar(producto, cantidad);
 
 
